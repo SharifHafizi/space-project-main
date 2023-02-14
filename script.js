b6d69a0b55308b54263f00a8e85196bb360d33fd
SharifHafizi <112721514+SharifHafizi@users.noreply.github.com>
2023-02-14 13:32:02 +0100

    added eventlistener on contact fomr button

--- script.js
+++ script.js
@@ -1,20 +1,6 @@
-function openForm() {
-  document.getElementById("popupForm").style.display = "block";
-}
-
-function closeForm(e) {
-  // e contains data about the click event
-  // e.target is what the eventlistener is on (popupForm)
-  // e.currentTarget is what we actually clicked!
-  // We gotta check that cause this also runs on popupForm's children, but we ONLY want the background
-  if (e.target === e.currentTarget) {
-    document.getElementById("popupForm").style.display = "none";
-  }
-}
-
 function windowLoaded(e) {
   // addEventListener setups a function (closeForm) to run on "click" event
-  document.getElementById("popupForm").addEventListener("click", closeForm);
+  // document.getElementById("popupForm").addEventListener("click", closeForm);
   document.getElementById("ticketForm").addEventListener("click", closeTicket);
 }
 
@@ -29,4 +15,17 @@ function closeTicket(e) {
   if (e.target === e.currentTarget) {
     document.getElementById("ticketForm").style.display = "none";
   }
+}
+function openForm() {
+  document.getElementById("popupForm").style.display = "block";
+}
+
+document.getElementById("contactsubmit").addEventListener("click", closeForm);
+
+function closeForm(e) {
+  document.getElementById("popupForm").style.display = "none";
+  // e contains data about the click event
+  // e.target is what the eventlistener is on (popupForm)
+  // e.currentTarget is what we actually clicked!
+  // We gotta check that cause this also runs on popupForm's children, but we ONLY want the background
 }
